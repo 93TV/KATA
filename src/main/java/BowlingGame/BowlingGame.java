9f6485b2aaaf423d0786f080acfd7bb1b1bded55
Thibault Viaene <thibault.viaene@student.odisee.be>
2023-10-05 08:40:43 +0200

    bowlingGame en BowlingGameTest aangemaakt

---
+++ src/main/java/BowlingGame/BowlingGame.java
@@ -0,0 +1,11 @@
+package BowlingGame;
+
+/**
+ * KATA : BowlingGame
+ *
+ * @author viaen
+ * @version 05/10/2023
+ */
+public class BowlingGame {
+
+}
---
+++ src/test/java/BowlingGame/BowlingGameTest.java
@@ -0,0 +1,20 @@
+package BowlingGame;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * KATA : BowlingGameTest
+ *
+ * @author viaen
+ * @version 05/10/2023
+ */
+class BowlingGameTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+}
